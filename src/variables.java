38d1efc1bf3e63512535c73f5cc7d7b18fd52c79
lcassani <lisandrocassani@gmail.com>
2023-07-24 20:05:24 -0300

    Prueba Variable

--- src/variables.java
+++ src/variables.java
@@ -1,6 +1,13 @@
 public class variables {
     public static void main (String [] args)   {
 
-        System.out.println("Hola Mundo");
+        byte variableByte;
+        byte variable2Byte;
+
+        variableByte = 100;
+        variable2Byte = 120;
+
+        System.out.println("Mostrando Suma Variables: " + variableByte + variable2Byte);
+
 }
     }
